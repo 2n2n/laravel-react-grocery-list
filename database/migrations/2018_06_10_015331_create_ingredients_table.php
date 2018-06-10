015ebf3628ddf59aee20b88aff1f1668c3c2acd9
Anthony Yolach Lloveras <anthonylloveras.boxtypd@gmail.com>
2018-06-10 09:57:46 +0800

    commit migrations and resource for ingredients

---
+++ database/migrations/2018_06_10_015331_create_ingredients_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
