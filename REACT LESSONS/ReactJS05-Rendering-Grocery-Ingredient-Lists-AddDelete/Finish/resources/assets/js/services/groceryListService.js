2773b3dfe53d9e522218e4b14dc97b066ac5d133
Sonny R. Recio <reciosonny@gmail.com>
2018-06-16 01:47:41 +0800

    Lesson 05

---
+++ REACT LESSONS/ReactJS05-Rendering-Grocery-Ingredient-Lists-AddDelete/Finish/resources/assets/js/services/groceryListService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export function addGroceryItem(ingredientId) {
+    
+    return axios.post('/api/grocery-lists/', {
+        ingredient_id: ingredientId
+    })
+        .then((response) => response.data)
+        .catch((e) => console.log('groceryListservice error', e.getMessage()));
+}
+
+export function deleteGroceryItem(ingredientId) {
+    return axios.delete('/api/grocery-lists/' + ingredientId)
+        .then((response) => response.data)
+    .catch((e) => console.log('groceryListService error', e.getMessage()))
+}
+
+
+export function getAllGroceryList() {
+    return axios.get('/api/grocery-lists/')
+        .then((response) => response.data)
+        .catch((e) => console.log('groceryListservice error', e.getMessage()));
+
+}
+
+
+
+export function clearGroceryList() {
+    return axios.delete('/api/grocery-lists-clear')
+        .then((respose) => response.data)
+        .catch( (e) => console.log('groceryListService error', e.getMessage() ))
+}
+
+
+
+
+
---
+++ REACT LESSONS/ReactJS05-Rendering-Grocery-Ingredient-Lists-AddDelete/Finish/resources/assets/js/services/menuIngredientService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export function fetchMenuIngredient(menuId) {
+    return axios.get('/api/menu/'+ menuId +'/ingredient')
+        .then((data) => data.data)
+        .catch((e) => console.log('menuIngredientService Error:', e.getMessage()));
+}
+
+export function addMenuIngredient(menuId, ingredientName) {
+    return axios.post('/api/menu/' + menuId + '/ingredient', {
+        name: ingredientName
+    })
+        .then((data) => data.data)
+        .catch((e) => console.log('menuIngredientService Error: ', e.getMessage()));
+}
+
+export function removeMenuIngredient(menuId, ingredientId) {
+    return axios.delete('/api/menu/' + menuId + '/ingredient/' + ingredientId)
+        .then((data) => data.data)
+        .catch( (e) => cosole.log('menuIngredientService Error: ', e.getMessage()) )
+}
